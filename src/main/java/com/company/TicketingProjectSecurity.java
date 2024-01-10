d582c7d4463929b1786acabb9380bec0bb1175d3
sa-023 <mavisky.023@gmail.com>
2024-01-09 21:30:51 -0600

    Initial commit

---
+++ src/main/java/com/company/TicketingProjectSecurity.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingProjectSecurity {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingProjectSecurity.class, args);
+    }
+
+}
